d64e83a6d5400cd13d62bcd0286947b49a7d9f52
mbarek-hani <mbarekhani30@gmail.com>
2025-07-08 02:13:14 +0100

    add support for dark/light theme

--- templates/pager.php
+++ templates/pager.php
@@ -1,17 +1,17 @@
-<ul>
+<ul class="pagination-container">
     <?php if ($current_page > 1) : ?>
-        <li>
+        <li class="pagination-item">
             <a href="<?= $url ?>?<?php echo !empty($search_query) ? 'q=' . $search_query . '&' : '' ?>page=<?= $current_page - 1 ?>&size=<?= $size ?>">&laquo;</a>
         </li>
     <?php endif; ?>
 
     <?php for($i = 1; $i <= $total_pages; $i++): ?>
-        <li>
+        <li class="pagination-item">
             <a href="<?= $url ?>?<?php echo !empty($search_query) ? 'q=' . $search_query . '&' : '' ?>page=<?= $i ?>&size=<?= $size ?>"<?php echo $i === $current_page ? " class='active'" : "" ?>><?= $i ?></a>
         </li>
     <?php endfor; ?>
     <?php if ($current_page < $total_pages ) : ?>
-        <li>
+        <li class="pagination-item">
             <a href="<?= $url ?>?<?php echo !empty($search_query) ? 'q=' . $search_query . '&' : '' ?>page=<?= $current_page + 1 ?>&size=<?= $size ?>">&raquo;</a>
         </li>
     <?php endif; ?>
