96c358c9ef26e9722b041e6433370cc24ceed527
mbarek-hani <mbarekhani30@gmail.com>
2025-08-07 01:18:59 +0100

    call die() instead of exit(1) in mh_request_terminate()

--- lib/request.php
+++ lib/request.php
@@ -2,32 +2,41 @@
 declare(strict_types=1);
 
 /*
-* terminate the current request with a given status code
-* @param $code the status code
-*/
-function mh_request_terminate(int $code): void {
+ * terminate the current request with a given status code
+ * @param $code the status code
+ */
+function mh_request_terminate(int $code): void
+{
     http_response_code($code);
-    exit(1);
+    die();
 }
 
 /*
-* insure the request is made with a given method otherwise terminate it with 405 METHOD NOT ALLOWED and set the allow header
-* @param $method the method to allow
-*/
-function mh_request_assert_method(string $method): void {
-    if (!isset($_SERVER["REQUEST_METHOD"]) || $_SERVER["REQUEST_METHOD"] !== $method) {
+ * insure the request is made with a given method otherwise terminate it with 405 METHOD NOT ALLOWED and set the allow header
+ * @param $method the method to allow
+ */
+function mh_request_assert_method(string $method): void
+{
+    if (
+        !isset($_SERVER["REQUEST_METHOD"]) ||
+        $_SERVER["REQUEST_METHOD"] !== $method
+    ) {
         http_response_code(405);
-        header("Allow: " . $method);
+        header("Allow: $method");
         exit();
     }
 }
 
 /*
-* insure the request is made with a either one of the given methods otherwise terminate it with 405 METHOD NOT ALLOWED and set the allow header
-* @param $methods the methods to allow
-*/
-function mh_request_assert_methods(array $methods): void {
-    if (!isset($_SERVER["REQUEST_METHOD"]) || in_array($_SERVER["REQUEST_METHOD"], haystack: $methods)) {
+ * insure the request is made with a either one of the given methods otherwise terminate it with 405 METHOD NOT ALLOWED and set the allow header
+ * @param $methods the methods to allow
+ */
+function mh_request_assert_methods(array $methods): void
+{
+    if (
+        !isset($_SERVER["REQUEST_METHOD"]) ||
+        in_array($_SERVER["REQUEST_METHOD"], haystack: $methods)
+    ) {
         http_response_code(405);
         header("Allow: " . implode(", ", $methods));
         exit();
@@ -35,23 +44,23 @@ function mh_request_assert_methods(array $methods): void {
 }
 
 /*
-* validate a GET parameter to be an integer in the range of a given min and max and return it or a default if this parameter doesn't exist, otherwise terminate request with a status of 400 BAD REQUEST 
-* @param $parameter the GET parameter to validate
-* @param $min the minimum integer the GET parameter has to be to pass validation
-* @param $max the maximum integer the GET parameter has to be to pass validation
-* @param $default the integer to return (if it is provided) if the GET parameter doesn't exist at all 
-* @return int the GET paramter value if it has passed validation or $default if provided
-*/
-function mh_request_get_int_query_parameter(string $parameter, int $min, int $max, ?int $default=null): int {
-    $value = filter_input(
-        INPUT_GET, 
-        $parameter, 
-        FILTER_VALIDATE_INT, 
-        [
-            "options" => ["min_range" => $min, "max_range" => $max],
-            "flags" => FILTER_NULL_ON_FAILURE
-        ]
-    );
+ * validate a GET parameter to be an integer in the range of a given min and max and return it or a default if this parameter doesn't exist, otherwise terminate request with a status of 400 BAD REQUEST
+ * @param $parameter the GET parameter to validate
+ * @param $min the minimum integer the GET parameter has to be to pass validation
+ * @param $max the maximum integer the GET parameter has to be to pass validation
+ * @param $default the integer to return (if it is provided) if the GET parameter doesn't exist at all
+ * @return int the GET paramter value if it has passed validation or $default if provided
+ */
+function mh_request_get_int_query_parameter(
+    string $parameter,
+    int $min,
+    int $max,
+    ?int $default = null,
+): int {
+    $value = filter_input(INPUT_GET, $parameter, FILTER_VALIDATE_INT, [
+        "options" => ["min_range" => $min, "max_range" => $max],
+        "flags" => FILTER_NULL_ON_FAILURE,
+    ]);
 
     if (($value === false && $default === null) || $value === null) {
         mh_request_terminate(400);
@@ -61,23 +70,25 @@ function mh_request_get_int_query_parameter(string $parameter, int $min, int $ma
         return $default;
     }
 
-    return $value; 
+    return $value;
 }
 
 /*
-* check if the request method is the given one
-* @param $method the method to check for
-* @return bool true if it is, false if not
-*/
-function mh_request_is_method(string $method):bool {
+ * check if the request method is the given one
+ * @param $method the method to check for
+ * @return bool true if it is, false if not
+ */
+function mh_request_is_method(string $method): bool
+{
     return $_SERVER["REQUEST_METHOD"] === $method;
 }
 
 /*
-* redirect clients to the given url
-* @param $url the url to redirect to
-*/
-function mh_request_redirect(string $url): never {
+ * redirect clients to the given url
+ * @param $url the url to redirect to
+ */
+function mh_request_redirect(string $url): never
+{
     header("Location: $url");
     exit();
 }
