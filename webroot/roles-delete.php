5dc689a798b9c212b23be3c9c02668abb364348d
mbarek-hani <mbarekhani30@gmail.com>
2025-08-17 02:18:51 +0100

    add roles deleting functionnality

---
+++ webroot/roles-delete.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+require "../lib/errors.php";
+require "../lib/database.php";
+require "../lib/request.php";
+require "../lib/template.php";
+
+mh_request_assert_method("POST");
+
+$id = mh_request_get_int_query_parameter("id", 1, PHP_INT_MAX);
+
+$pdo = mh_database_get_connection();
+
+if (!mh_database_does_role_exists($pdo, $id)) {
+    http_response_code(400);
+    mh_template_render_404();
+    die();
+}
+
+$statement = $pdo->prepare("delete from roles where id=:id");
+$statement->bindValue(":id", $id);
+$statement->execute();
+
+mh_request_redirect("/roles");
