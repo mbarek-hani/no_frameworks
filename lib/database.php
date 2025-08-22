da233772eac326adfa0861a444d107c6b56642d5
mbarek-hani <mbarekhani30@gmail.com>
2025-08-22 10:23:18 +0100

    delete no longer used functions

--- lib/database.php
+++ lib/database.php
@@ -78,62 +78,6 @@ function mh_database_is_unique_column_value(
 }
 
 /*
- * check if a user exists with username
- * @param $pdo PDO object to connect to db
- * @param $username username to look for
- * @param $id if passed, the check will be done on every user that have a different id
- * @return bool true if exists, false if not
- */
-function mh_database_does_username_exist(
-    PDO $pdo,
-    string $username,
-    ?int $id = null,
-): bool {
-    if ($id) {
-        $statement = $pdo->prepare(
-            "select count(*) from users where username=:username and id<>:id",
-        );
-        $statement->bindValue(":username", $username, PDO::PARAM_STR);
-        $statement->bindValue(":id", $id, PDO::PARAM_INT);
-    } else {
-        $statement = $pdo->prepare(
-            "select count(*) from users where username=:username",
-        );
-        $statement->bindValue(":username", $username, PDO::PARAM_STR);
-    }
-    $statement->execute();
-    return $statement->fetchColumn(0) == 1;
-}
-
-/*
- * check if a user exists with email
- * @param $pdo PDO object to connect to db
- * @param $email email to look for
- * @param $id if passed, the check will be done on every user that have a different id
- * @return bool true if exists, false if not
- */
-function mh_database_does_email_exist(
-    PDO $pdo,
-    string $email,
-    ?int $id = null,
-): bool {
-    if ($id) {
-        $statement = $pdo->prepare(
-            "select count(*) from users where email=:email and id<>:id",
-        );
-        $statement->bindValue(":email", $email, PDO::PARAM_STR);
-        $statement->bindValue(":id", $id, PDO::PARAM_INT);
-    } else {
-        $statement = $pdo->prepare(
-            "select count(*) from users where email=:email",
-        );
-        $statement->bindValue(":email", $email, PDO::PARAM_STR);
-    }
-    $statement->execute();
-    return $statement->fetchColumn(0) == 1;
-}
-
-/*
  * check if a user exists with first and last names
  * @param $pdo PDO object to connect to db
  * @param $first_name first name to look for
@@ -160,62 +104,6 @@ function mh_database_does_name_exist(
         );
         $statement->bindValue(":first_name", $first_name, PDO::PARAM_STR);
         $statement->bindValue(":last_name", $last_name, PDO::PARAM_STR);
-    }
-    $statement->execute();
-    return $statement->fetchColumn(0) == 1;
-}
-
-/*
- * check if a role exists with name
- * @param $pdo PDO object to connect to db
- * @param $role_name name to look for
- * @param $id if passed, the check will be done on every role that have a different id
- * @return bool true if exists, false if not
- */
-function mh_database_does_role_name_exist(
-    PDO $pdo,
-    string $role_name,
-    ?int $id = null,
-): bool {
-    if ($id) {
-        $statement = $pdo->prepare(
-            "select count(*) from roles where name=:name and id<>:id",
-        );
-        $statement->bindValue(":name", $role_name, PDO::PARAM_STR);
-        $statement->bindValue(":id", $id, PDO::PARAM_INT);
-    } else {
-        $statement = $pdo->prepare(
-            "select count(*) from roles where name=:name",
-        );
-        $statement->bindValue(":name", $role_name, PDO::PARAM_STR);
-    }
-    $statement->execute();
-    return $statement->fetchColumn(0) == 1;
-}
-
-/*
- * check if an action exists with name
- * @param $pdo PDO object to connect to db
- * @param $action_name name to look for
- * @param $id if passed, the check will be done on every action that have a different id
- * @return bool true if exists, false if not
- */
-function mh_database_does_action_name_exist(
-    PDO $pdo,
-    string $action_name,
-    ?int $id = null,
-): bool {
-    if ($id) {
-        $statement = $pdo->prepare(
-            "select count(*) from actions where name=:name and id<>:id",
-        );
-        $statement->bindValue(":name", $action_name, PDO::PARAM_STR);
-        $statement->bindValue(":id", $id, PDO::PARAM_INT);
-    } else {
-        $statement = $pdo->prepare(
-            "select count(*) from actions where name=:name",
-        );
-        $statement->bindValue(":name", $action_name, PDO::PARAM_STR);
     }
     $statement->execute();
     return $statement->fetchColumn(0) == 1;
