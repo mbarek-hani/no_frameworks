2e1ef1316d414947b2f15bbdca41d3a99699a29d
mbarek-hani <mbarekhani30@gmail.com>
2025-08-23 09:05:13 +0100

    some refactoring

--- lib/dotenv.php
+++ lib/dotenv.php
@@ -2,17 +2,22 @@
 declare(strict_types=1);
 
 /*
-* load environment variables from a .env file
-* @param $path the path of the .env file
-* @throws Exception when $path doesn't exist
-*/
-function mh_dotenv_load(string $path):void {
-    if (!file_exists($path)) throw new Exception($path . " doesn't exist");
+ * load environment variables from a .env file
+ * @param $path the path of the .env file
+ * @throws Exception when $path doesn't exist
+ */
+function mh_dotenv_load(string $path): void
+{
+    if (!file_exists($path)) {
+        throw new Exception("$path doesn't exist");
+    }
 
     $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
     foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue;
-        list($name, $value) = explode('=', $line, 2);
+        if (strpos(trim($line), "#") === 0) {
+            continue;
+        }
+        [$name, $value] = explode("=", $line, 2);
         $name = trim($name);
         $value = trim($value);
 
