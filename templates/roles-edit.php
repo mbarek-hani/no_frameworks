6dd64aa9eec43c2bc5c187ddafb714573b97c856
mbarek-hani <mbarekhani30@gmail.com>
2025-09-02 19:52:57 +0100

    add: check authorization and actions when outputing the roles listing and role edit pages

--- templates/roles-edit.php
+++ templates/roles-edit.php
@@ -1,25 +1,24 @@
+<?php $canUpdate = mh_authorization_is_authorized("UpdateRole"); ?>
 <div class="content">
-    <form class="edit" method="POST" action="/roles/edit/<?= $role["id"] ?>">
+    <form class="edit" <?php if (
+                            $canUpdate
+                        ): ?> method="POST" action="/roles/edit/<?= $role["id"] ?>" <?php endif; ?>>
         <div>
             <label for="name">Name</label>
-            <input type="text" name="name" placeholder="name" value="<?= $role[
-                "name"
-            ] ?>" />
+            <input type="text" name="name" placeholder="name" value="<?= $role["name"] ?>" <?= !$canUpdate ? "readonly" : "" ?> />
         </div>
         <?php if (isset($errors["name"])): ?>
             <p class="error"><?= $errors["name"] ?></p>
         <?php endif; ?>
         <div>
             <label for="description">Description</label>
-            <input type="text" name="description" placeholder="Description..." value="<?= $role[
-                "description"
-            ] ?>" />
+            <input type="text" name="description" placeholder="Description..." value="<?= $role["description"] ?>" <?= !$canUpdate ? "readonly" : "" ?> />
         </div>
         <?php if (isset($errors["description"])): ?>
             <p class="error"><?= $errors["description"] ?></p>
         <?php endif; ?>
         <div>
-            <input type="submit" value="Edit" />
+            <input type="submit" value="Edit" <?= !$canUpdate ? "disabled" : "" ?> />
             <a href="/roles">Cancel</a>
         </div>
     </form>
@@ -30,7 +29,9 @@
                 <th width="5%">#</th>
                 <th width="20%">name</th>
                 <th width="65%">description</th>
-                <th width="10%">remove</th>
+                <?php if ($canUpdate): ?>
+                    <th width="10%">remove</th>
+                <?php endif; ?>
             </tr>
         </thead>
         <tbody>
@@ -40,46 +41,35 @@
                         <td><?= $action["id"] ?></td>
                         <td><?= $action["name"] ?></td>
                         <td><?= $action["description"] ?></td>
-                        <td class="actions">
-                            <form class="removeForm" action="/roles/edit/<?= $role[
-                                "id"
-                            ] ?>" method="POST" id="form<?= $action[
-    "id"
-] ?>" style="display: none;">
-                                <input type="hidden" name="do" value="remove" />
-                                <input type="hidden" name="action" value="<?= $action[
-                                    "id"
-                                ] ?>" />
-                            </form>
-                            <button type="submit" form="form<?= $action[
-                                "id"
-                            ] ?>">
-                                <img src="/assets/delete.svg" width="30" height="30" />
-                            </button>
-                        </td>
+                        <?php if ($canUpdate): ?>
+                            <td class="actions">
+                                <form class="removeForm" action="/roles/edit/<?= $role["id"] ?>" method="POST" id="form<?= $action["id"] ?>" style="display: none;">
+                                    <input type="hidden" name="do" value="remove" />
+                                    <input type="hidden" name="action" value="<?= $action["id"] ?>" />
+                                </form>
+                                <button type="submit" form="form<?= $action["id"] ?>">
+                                    <img src="/assets/delete.svg" width="30" height="30" />
+                                </button>
+                            </td>
+                        <?php endif; ?>
                     </tr>
                 <?php endforeach; ?>
             <?php else: ?>
                 <tr>
-                    <td colspan="4" style="text-align: center;">There are no actions associated with role "<?= $role[
-                        "name"
-                    ] ?>".</td>
+                    <td colspan="4" style="text-align: center;">There are no actions associated with role "<?= $role["name"] ?>".</td>
                 </tr>
             <?php endif; ?>
         </tbody>
     </table>
-    <form class="add-role" method="POST" action="/roles/edit/<?= $role[
-        "id"
-    ] ?>">
-        <input type="hidden" name="do" value="add" />
-        <div>
-            <label for="action">action</label>
+    <?php if ($canUpdate): ?>
+        <form class="add-role" method="POST" action="/roles/edit/<?= $role["id"] ?>">
+            <input type="hidden" name="do" value="add" />
+            <div>
+                <label for="action">action</label>
                 <?php if (count($other_actions) > 0): ?>
                     <select id="action" name="action">
                         <?php foreach ($other_actions as $action): ?>
-                            <option value="<?= $action["id"] ?>"><?= $action[
-    "name"
-] ?></option>
+                            <option value="<?= $action["id"] ?>"><?= $action["name"] ?></option>
                         <?php endforeach; ?>
                     </select>
                     <input type="submit" value="Add" />
@@ -87,20 +77,21 @@
                     <select disabled>
                         <option>No actions found.</option>
                     </select>
-                    <input type="submit" value="Add" disabled/>
+                    <input type="submit" value="Add" disabled />
                 <?php endif; ?>
-        </div>
-    </form>
+            </div>
+        </form>
+    <?php endif; ?>
 </div>
-<script>
-    const removeForms = document.querySelectorAll(".removeForm");
-    for (let i = 0; i<removeForms.length; i++) {
-        removeForms[i].addEventListener("submit", (e) => {
-            if(!confirm("Are you sure you want to remove this action from role \"<?= $role[
-                "name"
-            ] ?>\"?")) {
-                e.preventDefault();
-            }
-        });
-    }
-</script>
+<?php if ($canUpdate): ?>
+    <script>
+        const removeForms = document.querySelectorAll(".removeForm");
+        for (let i = 0; i < removeForms.length; i++) {
+            removeForms[i].addEventListener("submit", (e) => {
+                if (!confirm("Are you sure you want to remove this action from role \"<?= $role["name"] ?>\"?")) {
+                    e.preventDefault();
+                }
+            });
+        }
+    </script>
+<?php endif; ?>
--- templates/roles.php
+++ templates/roles.php
@@ -1,8 +1,8 @@
 <div class="content">
     <form class="search" method="get" action="/roles">
         <input type="text" name="q" placeholder="Search with role name..." value="<?= htmlspecialchars(
-            $search_query,
-        ) ?>"/> <input type="submit" value="search" />
+                                                                                        $search_query,
+                                                                                    ) ?>" /> <input type="submit" value="search" />
     </form>
     <table>
         <thead>
@@ -10,7 +10,14 @@
                 <th width="5%">#</th>
                 <th width="20%">name</th>
                 <th width="65%">description</th>
-                <th width="10%">actions</th>
+                <?php if (
+                    mh_authorization_is_authorized_any([
+                        "ReadRole",
+                        "DeleteRole",
+                    ])
+                ): ?>
+                    <th width="10%">actions</th>
+                <?php endif; ?>
             </tr>
         </thead>
         <tbody>
@@ -20,19 +27,26 @@
                         <td><?= $role["id"] ?></td>
                         <td><?= $role["name"] ?></td>
                         <td><?= $role["description"] ?></td>
-                        <td class="actions">
-                            <a href="/roles/edit/<?= $role["id"] ?>">
-                                <img src="/assets/edit.svg" width="30" height="30" />
-                            </a>
-                            <form class="deleteForm" id="form<?= $role[
-                                "id"
-                            ] ?>" action="/roles/delete/<?= $role[
-    "id"
-] ?>" method="POST" style="display: none;"></form>
-                            <button form="form<?= $role["id"] ?>" type="submit">
-                                <img src="assets/delete.svg" width="30" height="30" />
-                            </button>
-                        </td>
+                        <?php if (
+                            mh_authorization_is_authorized_any([
+                                "ReadUser",
+                                "DeleteUser",
+                            ])
+                        ): ?>
+                            <td class="actions">
+                                <?php if (mh_authorization_is_authorized("ReadRole")): ?>
+                                    <a href="/roles/edit/<?= $role["id"] ?>">
+                                        <img src="/assets/edit.svg" width="30" height="30" />
+                                    </a>
+                                <?php endif; ?>
+                                <?php if (mh_authorization_is_authorized("DeleteRole")): ?>
+                                    <form class="deleteForm" id="form<?= $role["id"] ?>" action="/roles/delete/<?= $role["id"] ?>" method="POST" style="display: none;"></form>
+                                    <button form="form<?= $role["id"] ?>" type="submit">
+                                        <img src="assets/delete.svg" width="30" height="30" />
+                                    </button>
+                                <?php endif; ?>
+                            </td>
+                        <?php endif; ?>
                     </tr>
                 <?php endforeach; ?>
             <?php else: ?>
@@ -50,13 +64,15 @@
         $current_page,
     ); ?>
 </div>
-<script>
+<?php if (mh_authorization_is_authorized("DeleteRole")): ?>
+    <script>
         const deleteForms = document.querySelectorAll(".deleteForm");
-        for (let i = 0; i<deleteForms.length; i++) {
+        for (let i = 0; i < deleteForms.length; i++) {
             deleteForms[i].addEventListener("submit", (e) => {
-                if(!confirm("Are you sure you want to delete this role?")) {
+                if (!confirm("Are you sure you want to delete this role?")) {
                     e.preventDefault();
                 }
             });
         }
     </script>
+<?php endif; ?>
