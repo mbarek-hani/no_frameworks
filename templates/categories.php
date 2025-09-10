5eb288f0dde702f5d6635aa703815edaaeda6694
mbarek-hani <mbarekhani30@gmail.com>
2025-09-10 02:14:37 +0100

    display the categories in a simple tree like structure

--- templates/categories.php
+++ templates/categories.php
@@ -1,3 +1,11 @@
 <div class="content">
-    <p>This is the categories listing page.</p>
+    <?php if (count($categories) > 0): ?>
+        <ul>
+            <?php foreach ($categories as $category): ?>
+                <li>
+                    <?= str_repeat("&nbsp;", intval($category["depth"]) * 4) . $category["name"] ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    <?php endif; ?>
 </div>
--- webroot/categories.php
+++ webroot/categories.php
@@ -8,16 +8,23 @@ require_once "../lib/authorization.php";
 require_once "../lib/database.php";
 require_once "../lib/request.php";
 require_once "../lib/template.php";
-require_once "../lib/validate.php";
 
 mh_request_assert_method("GET");
+
+$logged_in_user = mh_authentication_require_login();
 
 function mh_render_categories(array $categories): void
 {
     require_once "../templates/categories.php";
 }
 
+$pdo = mh_database_get_connection();
+
+$statement = $pdo->query("select node.id, node.name, count(parent.name) -1 as depth from categories as node, categories as parent where node.lft between parent.lft and parent.rgt group by node.name order by node.lft");
+$categories = mh_template_escape_array_of_arrays($statement->fetchAll(PDO::FETCH_ASSOC));
+
+
 mh_template_render_header("Categories");
 mh_template_render_sidebar("/categories");
-mh_render_categories([]);
+mh_render_categories($categories);
 mh_template_render_footer();
