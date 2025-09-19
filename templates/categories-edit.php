ecf60da7421d3ac69d7e12a5075283d019b2b233
mbarek-hani <mbarekhani30@gmail.com>
2025-09-19 17:48:29 +0100

    implement some parts of the categories edit functionnality

--- templates/categories-edit.php
+++ templates/categories-edit.php
@@ -8,6 +8,23 @@
             <p class="error"><?= $errors["name"] ?></p>
         <?php endif; ?>
         <div>
+            <label for="parent">Parent category</label>
+            <?php if (count($categories) > 0): ?>
+                <select id="parent" name="parent_id" class="input">
+                    <?php foreach ($categories as $cat): ?>
+                        <option value="<?= $cat["id"] ?>" <?= $cat["id"] == $parent_category["id"] ? "selected" : "" ?>><?= str_repeat("&nbsp;&nbsp;", $cat["depth"]) . $cat["name"] ?></option>
+                    <?php endforeach; ?>
+                </select>
+            <?php else: ?>
+                <select disabled>
+                    <option>No categories found.</option>
+                </select>
+            <?php endif; ?>
+        </div>
+        <?php if (isset($errors["parent"])): ?>
+            <p class="error"><?= $errors["parent"] ?></p>
+        <?php endif; ?>
+        <div>
             <input type="submit" value="Edit" />
             <a href="/categories">Cancel</a>
         </div>
--- webroot/categories-edit.php
+++ webroot/categories-edit.php
@@ -7,13 +7,14 @@ require_once "../lib/authentication.php";
 require_once "../lib/authorization.php";
 require_once "../lib/database.php";
 require_once "../lib/request.php";
+require_once "../lib/validate.php";
 require_once "../lib/template.php";
 
 mh_request_assert_methods(["GET", "POST"]);
 
 $logged_in_user = mh_authentication_require_login();
 
-function mh_render_categories_edit(array $category): void
+function mh_render_categories_edit(array $category, array $categories, array $errors, array $parent_category): void
 {
     require_once "../templates/categories-edit.php";
 }
@@ -31,22 +32,51 @@ if (!mh_database_does_row_exist($pdo, "categories", $category_id)) {
 $category = null;
 $errors = [
     "name" => null,
+    "parent_id" => null,
 ];
 $edited_category = null;
 
+$statement = $pdo->query("select node.id, node.name, count(parent.name) -1 as depth, node.lft, node.rgt from categories as node, categories as parent where node.lft between parent.lft and parent.rgt group by node.name order by node.lft");
+$categories = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$statement = $pdo->prepare("select parent.id, parent.name, parent.lft, parent.rgt from categories as node, categories as parent where node.lft between parent.lft and parent.rgt and node.id = :id order by parent.lft desc");
+$statement->bindValue(":id", $category_id, PDO::PARAM_INT);
+$statement->execute();
+$result = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+$parent_category = count($result) > 1 ? $result[1] : $result[0];
+
 if (mh_request_is_method("GET")) {
-    $statement = $pdo->prepare("select * from categories where id = :id");
-    $statement->bindValue(":id", $category_id, PDO::PARAM_INT);
-    $statement->execute();
-    $category = $statement->fetch(PDO::FETCH_ASSOC);
+    $category = array_values(array_filter($categories, fn($c) => $c["id"] === $category_id))[0];
 } else {
-    // TODO
+    $edited_category = [
+        "id" => $category_id,
+        "name" => trim($_POST["name"] ?? ""),
+        "parent_id" => mh_request_get_int_parameter("parent_id", INPUT_POST, 1, PHP_INT_MAX, 0),
+    ];
+
+    $errors["name"] = mh_validate_category_name($edited_category["name"], "name");
+    $errors["parent_id"] = $edited_category["parent_id"] === 0 ? "parent category is required" : null;
+
+    if ($errors["name"] === null && !mh_database_is_unique_column_value($pdo, "categories", "name", $edited_category["name"], $category_id)) {
+        $errors["name"] = "name already in use";
+    }
+
+    if (mh_errors_is_empty($errors)) {
+        $statement = $pdo->prepare("update categories set name=:name where id=:id");
+        $statement->bindValue(":name", $edited_category["name"], PDO::PARAM_STR);
+        $statement->bindValue(":id", $edited_category["id"], PDO::PARAM_INT);
+        $statement->execute();
+
+        mh_request_redirect("/categories");
+    }
 }
 
 
-$data = mh_template_escape_array($category ?? $edited_category);
+$category = mh_template_escape_array($category ?? $edited_category);
+$categories = mh_template_escape_array_of_arrays($categories);
 
 mh_template_render_header("Add category");
 mh_template_render_sidebar("/categories");
-mh_render_categories_edit($category);
+mh_render_categories_edit($category, $categories,  $errors, $parent_category);
 mh_template_render_footer();
