f2066229117881c14f537d5dbd677af358538602
mbarek-hani <mbarekhani30@gmail.com>
2025-11-18 16:37:03 +0100

    define mh_users_get_by_id, mh_users_get_roles, mh_users_get_other_roles, mh_users_add_role, mh_users_remove_role, mh_users_edit inside of webroot/users.php instead of lib/users.php as they are used only there, and delete lib/users.php because it is no longer needed

--- webroot/users-edit.php
+++ webroot/users-edit.php
@@ -9,7 +9,6 @@ require_once "../lib/request.php";
 require_once "../lib/database.php";
 require_once "../lib/validate.php";
 require_once "../lib/template.php";
-require_once "../lib/users.php";
 
 mh_request_assert_methods(["GET", "POST"]);
 
@@ -24,6 +23,92 @@ function mh_render_users_edit(
     array $errors,
 ): void {
     require "../templates/users-edit.php";
+}
+
+function mh_users_get_by_id(PDO $pdo, int $user_id): array
+{
+    $statement = $pdo->prepare(
+        "select id, username, first_name, last_name, email from users where id = :id",
+    );
+    $statement->bindValue(":id", $user_id, PDO::PARAM_INT);
+    $statement->execute();
+    return $statement->fetch(PDO::FETCH_ASSOC);
+}
+
+function mh_users_get_roles(PDO $pdo, int $user_id): array
+{
+    $statement = $pdo->prepare(
+        "select id, name, description from roles, users_roles where role_id = id and user_id = :id",
+    );
+    $statement->bindValue(":id", $user_id, PDO::PARAM_INT);
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function mh_users_get_other_roles(PDO $pdo, int $user_id): array
+{
+    $statement = $pdo->prepare(
+        "select id, name, description from roles where id not in (select role_id from users_roles where user_id = :id)",
+    );
+    $statement->bindValue(":id", $user_id, PDO::PARAM_INT);
+    $statement->execute();
+    return $statement->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function mh_users_add_role(PDO $pdo, int $user_id, int $role_id): void
+{
+    $statement = $pdo->prepare(
+        "insert into users_roles values(:user_id, :role_id)",
+    );
+    $statement->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+    $statement->bindValue(":role_id", $role_id, PDO::PARAM_INT);
+    $statement->execute();
+}
+
+function mh_users_remove_role(PDO $pdo, int $user_id, int $role_id): void
+{
+    $statement = $pdo->prepare(
+        "delete from users_roles where user_id=:user_id and role_id=:role_id",
+    );
+    $statement->bindValue(":user_id", $user_id, PDO::PARAM_INT);
+    $statement->bindValue(":role_id", $role_id, PDO::PARAM_INT);
+    $statement->execute();
+}
+
+function mh_users_edit(PDO $pdo, array $edited_user): void
+{
+    if (!empty($edited_user["password"])) {
+        $statement = $pdo->prepare(
+            "update users set username=:username, first_name=:first_name, last_name=:last_name, email=:email, password=:password where id=:id",
+        );
+        $password_hash = password_hash(
+            $edited_user["password"],
+            PASSWORD_DEFAULT,
+        );
+        $statement->bindValue(":password", $password_hash, Pdo::PARAM_STR);
+    } else {
+        $statement = $pdo->prepare(
+            "update users set username=:username, first_name=:first_name, last_name=:last_name, email=:email where id=:id",
+        );
+    }
+    $statement->bindValue(
+        ":username",
+        $edited_user["username"],
+        PDO::PARAM_STR,
+    );
+    $statement->bindValue(
+        ":first_name",
+        $edited_user["first_name"],
+        PDO::PARAM_STR,
+    );
+    $statement->bindValue(
+        ":last_name",
+        $edited_user["last_name"],
+        PDO::PARAM_STR,
+    );
+    $statement->bindValue(":email", $edited_user["email"], PDO::PARAM_STR);
+    $statement->bindValue(":id", $edited_user["id"], PDO::PARAM_INT);
+    $statement->execute();
 }
 
 $user_id = mh_request_get_int_parameter("id", INPUT_GET, 1, PHP_INT_MAX);
@@ -54,11 +139,11 @@ if (mh_request_is_method("GET")) {
     $user = mh_users_get_by_id($pdo, $user_id);
 
     $user_roles = mh_template_escape_array_of_arrays(
-        mh_users_get_roles($pdo, $user_id)
+        mh_users_get_roles($pdo, $user_id),
     );
 
     $other_roles = mh_template_escape_array_of_arrays(
-        mh_users_get_other_roles($pdo, $user_id)
+        mh_users_get_other_roles($pdo, $user_id),
     );
 } else {
     mh_authorization_assert_authorized("UpdateUser");
@@ -113,7 +198,11 @@ if (mh_request_is_method("GET")) {
         $errors["email"] = mh_validate_email($edited_user["email"], "email");
 
         if (!empty($edited_user["password"])) {
-            $errors["password"] = mh_validate_password($edited_user["password"], $edited_user["password_confirm"], "password");
+            $errors["password"] = mh_validate_password(
+                $edited_user["password"],
+                $edited_user["password_confirm"],
+                "password",
+            );
         }
 
         if (
